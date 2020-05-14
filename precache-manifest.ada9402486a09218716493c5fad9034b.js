31e955a5ab59f53c771ad5a81faa5f26a1e7d7a2
Kerwin Hy <kerwin.hy@gmail.com>
2020-05-14 10:07:19 -0700

    Updates

---
+++ precache-manifest.ada9402486a09218716493c5fad9034b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0d43681922bea3fcaed322398f714c",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "2e2c87cb4a89ae62692c",
+    "url": "/clickygame/static/css/main.15870bc5.chunk.css"
+  },
+  {
+    "revision": "7a489e320a3eda2f9f63",
+    "url": "/clickygame/static/js/2.b04b7118.chunk.js"
+  },
+  {
+    "revision": "2e2c87cb4a89ae62692c",
+    "url": "/clickygame/static/js/main.65626311.chunk.js"
+  },
+  {
+    "revision": "725b263c1d129add8883",
+    "url": "/clickygame/static/js/runtime-main.353be89b.js"
+  }
+]);
